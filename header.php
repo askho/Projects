6caba2d607a78e2d4da1c5e683f6c41c3b28ed74
Leon <ho.leon@yahoo.com>
2014-05-02 12:01:54 -0700

    Removed jquery

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="jquerymobile1.4.2/jquery.mobile-1.4.2.css">
+<script src="jquery-2.1.1.min.js"></script>
+<script src="jquerymobile1.4.2/jquery.mobile-1.4.2.js"></script>
+</head>
+<body>
+
+<div data-role="page" id="pageone">
+  <div data-role="header">
+  <h1>Welcome To My Homepage</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+  <p>Welcome!</p>
+  </div>
+
+  <div data-role="footer">
+  <h1>Footer Text</h1>
+  </div>
+</div> 
+
+</body>
+</html>
