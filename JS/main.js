67484629babdf437af9b47907f6ce38e5a33e1fc
Logan Hollins <logan.hollins1@gmail.com>
2014-05-07 09:37:52 -0700

    fixed button issues
    fixed button issues

--- JS/main.js
+++ JS/main.js
@@ -56,11 +56,24 @@ function loadMap() {
     + 'callback=initializeMap';
   document.body.appendChild(script);
 }
-
+var moncheck = 0;
+var tuecheck = 0;
+var wedcheck = 0;
+var thucheck = 0;
+var fricheck = 0;
+var satcheck = 0;
+var suncheck = 0;
 //Works until you look at the page
 //maybe needs refresh function?
 function radioCheck() {
+  var bad = 0;
   if ($('#driver').is(":checked") == false && $('#passenger').is(":checked") == false) {
+    document.getElementById('errorM').innerHTML = 'Please choose a role.';
+    bad = 1;
+  }
+  else {
+    document.getElementById('errorM').innerHTML = '';
+    bad = 0;
   }
   if ($('#driver').is(":checked") == true) {
     $("#driver1").attr("checked",true);
@@ -81,80 +94,45 @@ function radioCheck() {
     $("#passenger6").attr("checked",true);
     $("#passenger7").attr("checked",true);  }
   var checked = 0;
+  var last;
   //used to check if anything has been checked.  If nothing is checked wont proceed.
   if ($("#mondayC").is(":checked")) {
     checked++;
-    $("#head1").append('<a href="#monday" data-role="button">Mon</a>');
-    $("#head2").append('<a href="#monday" data-role="button">Mon</a>');
-    $("#head3").append('<a href="#monday" data-role="button">Mon</a>');
-    $("#head4").append('<a href="#monday" data-role="button">Mon</a>');
-    $("#head5").append('<a href="#monday" data-role="button">Mon</a>');
-    $("#head6").append('<a href="#monday" data-role="button">Mon</a>');
-    $("#head7").append('<a href="#monday" data-role="button">Mon</a>');
+    last = 0;
   }
   if ($('#tuesdayC').is(":checked")) {
     checked++;
-    $("#head1").append('<a href="#tuesday" data-role="button">Tue</a>');
-    $("#head2").append('<a href="#tuesday" data-role="button">Tue</a>');
-    $("#head3").append('<a href="#tuesday" data-role="button">Tue</a>');
-    $("#head4").append('<a href="#tuesday" data-role="button">Tue</a>');
-    $("#head5").append('<a href="#tuesday" data-role="button">Tue</a>');
-    $("#head6").append('<a href="#tuesday" data-role="button">Tue</a>');
-    $("#head7").append('<a href="#tuesday" data-role="button">Tue</a>');
+    last = 1;
   }
   if ($('#wednesdayC').is(":checked")) {
     checked++;
-    $("#head1").append('<a href="#wednesday" data-role="button">Wed</a>');
-    $("#head2").append('<a href="#wednesday" data-role="button">Wed</a>');
-    $("#head3").append('<a href="#wednesday" data-role="button">Wed</a>');
-    $("#head4").append('<a href="#wednesday" data-role="button">Wed</a>');
-    $("#head5").append('<a href="#wednesday" data-role="button">Wed</a>');
-    $("#head6").append('<a href="#wednesday" data-role="button">Wed</a>');
-    $("#head7").append('<a href="#wednesday" data-role="button">Wed</a>');
+    last = 2;
   }
   if ($('#thursdayC').is(":checked")) {
     checked++;
-    $("#head1").append('<a href="#thursday" data-role="button">Thu</a>');
-    $("#head2").append('<a href="#thursday" data-role="button">Thu</a>');
-    $("#head3").append('<a href="#thursday" data-role="button">Thu</a>');
-    $("#head4").append('<a href="#thursday" data-role="button">Thu</a>');
-    $("#head5").append('<a href="#thursday" data-role="button">Thu</a>');
-    $("#head6").append('<a href="#thursday" data-role="button">Thu</a>');
-    $("#head7").append('<a href="#thursday" data-role="button">Thu</a>');
+    last = 3;
   }
   if ($('#fridayC').is(":checked")) {
     checked++;
-    $("#head1").append('<a href="#friday" data-role="button">Fri</a>');
-    $("#head2").append('<a href="#friday" data-role="button">Fri</a>');
-    $("#head3").append('<a href="#friday" data-role="button">Fri</a>');
-    $("#head4").append('<a href="#friday" data-role="button">Fri</a>');
-    $("#head5").append('<a href="#friday" data-role="button">Fri</a>');
-    $("#head6").append('<a href="#friday" data-role="button">Fri</a>');
-    $("#head7").append('<a href="#friday" data-role="button">Fri</a>');
+    last = 4;
   }
   if ($('#saturdayC').is(":checked")) {
     checked++;
-    $("#head1").append('<a href="#saturday" data-role="button">Sat</a>');
-    $("#head2").append('<a href="#saturday" data-role="button">Sat</a>');
-    $("#head3").append('<a href="#saturday" data-role="button">Sat</a>');
-    $("#head4").append('<a href="#saturday" data-role="button">Sat</a>');
-    $("#head5").append('<a href="#saturday" data-role="button">Sat</a>');
-    $("#head6").append('<a href="#saturday" data-role="button">Sat</a>');
-    $("#head7").append('<a href="#saturday" data-role="button">Sat</a>');
+    last = 5
   }
   if ($('#sundayC').is(":checked")) {
     checked++;
-    $("#head1").append('<a href="#sunday" data-role="button">Sun</a>');
-    $("#head2").append('<a href="#sunday" data-role="button">Sun</a>');
-    $("#head3").append('<a href="#sunday" data-role="button">Sun</a>');
-    $("#head4").append('<a href="#sunday" data-role="button">Sun</a>');
-    $("#head5").append('<a href="#sunday" data-role="button">Sun</a>');
-    $("#head6").append('<a href="#sunday" data-role="button">Sun</a>');
-    $("#head7").append('<a href="#sunday" data-role="button">Sun</a>');
+    last = 6;
   }
+
+  
   if (checked == 0) {
-    alert('Please select a day');
+    document.getElementById('errorA').innerHTML = 'Please select a day.';
+        }
+  else {
+    document.getElementById('errorA').innerHTML = '';
   }
+
   var location = document.getElementById('changeMe');
 
     if ($('#sundayC').is(":checked")) {
@@ -178,9 +156,110 @@ function radioCheck() {
     if ($('#mondayC').is(":checked")) { 
     location.setAttribute('href', '#monday');
   }
+
+  if (bad == 1 || checked == 0) {
+    return false;
+  }
+  else {
+      switch (last) {
+case 0:
+    $("#submit1").append('<input type="submit" data-inline="true" value="Submit"/>');
+    break;
+case 1:
+    $("#submit2").append('<input type="submit" data-inline="true" value="Submit"/>');
+    break;
+case 2:
+    $("#submit3").append('<input type="submit" data-inline="true" value="Submit"/>');
+    break;
+case 3:
+    $("#submit4").append('<input type="submit" data-inline="true" value="Submit"/>');
+    break;
+case 4:
+    $("#submit5").append('<input type="submit" data-inline="true" value="Submit"/>');
+    break;
+case 5:
+    $("#submit6").append('<input type="submit" data-inline="true" value="Submit"/>');
+    break;
+case 6:
+    $("#submit7").append('<input type="submit" data-inline="true" value="Submit"/>');
+    break;
 }
 
+if ($("#mondayC").is(":checked") && moncheck == 0) {
+    $("#head1").append('<a href="#monday" data-role="button">Mon</a>');
+    $("#head2").append('<a href="#monday" data-role="button">Mon</a>');
+    $("#head3").append('<a href="#monday" data-role="button">Mon</a>');
+    $("#head4").append('<a href="#monday" data-role="button">Mon</a>');
+    $("#head5").append('<a href="#monday" data-role="button">Mon</a>');
+    $("#head6").append('<a href="#monday" data-role="button">Mon</a>');
+    $("#head7").append('<a href="#monday" data-role="button">Mon</a>');
+    moncheck++;
+}
+if ($("#tuesdayC").is(":checked") && tuecheck == 0) {
+    $("#head1").append('<a href="#tuesday" data-role="button">Tue</a>');
+    $("#head2").append('<a href="#tuesday" data-role="button">Tue</a>');
+    $("#head3").append('<a href="#tuesday" data-role="button">Tue</a>');
+    $("#head4").append('<a href="#tuesday" data-role="button">Tue</a>');
+    $("#head5").append('<a href="#tuesday" data-role="button">Tue</a>');
+    $("#head6").append('<a href="#tuesday" data-role="button">Tue</a>');
+    $("#head7").append('<a href="#tuesday" data-role="button">Tue</a>');
+    tuecheck++;
+}
+if ($("#wednesdayC").is(":checked") && wedcheck == 0) {
+    $("#head1").append('<a href="#wednesday" data-role="button">Wed</a>');
+    $("#head2").append('<a href="#wednesday" data-role="button">Wed</a>');
+    $("#head3").append('<a href="#wednesday" data-role="button">Wed</a>');
+    $("#head4").append('<a href="#wednesday" data-role="button">Wed</a>');
+    $("#head5").append('<a href="#wednesday" data-role="button">Wed</a>');
+    $("#head6").append('<a href="#wednesday" data-role="button">Wed</a>');
+    $("#head7").append('<a href="#wednesday" data-role="button">Wed</a>');
+    wedcheck++;
+}
 
+if ($("#thursdayC").is(":checked") && thucheck == 0) {
+    $("#head1").append('<a href="#thursday" data-role="button">Thu</a>');
+    $("#head2").append('<a href="#thursday" data-role="button">Thu</a>');
+    $("#head3").append('<a href="#thursday" data-role="button">Thu</a>');
+    $("#head4").append('<a href="#thursday" data-role="button">Thu</a>');
+    $("#head5").append('<a href="#thursday" data-role="button">Thu</a>');
+    $("#head6").append('<a href="#thursday" data-role="button">Thu</a>');
+    $("#head7").append('<a href="#thursday" data-role="button">Thu</a>');
+    thucheck++;
+  }
+
+if ($("#fridayC").is(":checked") && fricheck == 0) {
+    $("#head1").append('<a href="#friday" data-role="button">Fri</a>');
+    $("#head2").append('<a href="#friday" data-role="button">Fri</a>');
+    $("#head3").append('<a href="#friday" data-role="button">Fri</a>');
+    $("#head4").append('<a href="#friday" data-role="button">Fri</a>');
+    $("#head5").append('<a href="#friday" data-role="button">Fri</a>');
+    $("#head6").append('<a href="#friday" data-role="button">Fri</a>');
+    $("#head7").append('<a href="#friday" data-role="button">Fri</a>');
+    fricheck++
+}
+
+if ($("#saturdayC").is(":checked") && satcheck == 0) {
+    $("#head1").append('<a href="#saturday" data-role="button">Sat</a>');
+    $("#head2").append('<a href="#saturday" data-role="button">Sat</a>');
+    $("#head3").append('<a href="#saturday" data-role="button">Sat</a>');
+    $("#head4").append('<a href="#saturday" data-role="button">Sat</a>');
+    $("#head5").append('<a href="#saturday" data-role="button">Sat</a>');
+    $("#head6").append('<a href="#saturday" data-role="button">Sat</a>');
+    $("#head7").append('<a href="#saturday" data-role="button">Sat</a>');
+    satcheck++;
+}
+if ($("#sundayC").is(":checked") && suncheck == 0) {
+    $("#head1").append('<a href="#sunday" data-role="button">Sun</a>');
+    $("#head2").append('<a href="#sunday" data-role="button">Sun</a>');
+    $("#head3").append('<a href="#sunday" data-role="button">Sun</a>');
+    $("#head4").append('<a href="#sunday" data-role="button">Sun</a>');
+    $("#head5").append('<a href="#sunday" data-role="button">Sun</a>');
+    $("#head6").append('<a href="#sunday" data-role="button">Sun</a>');
+    $("#head7").append('<a href="#sunday" data-role="button">Sun</a>');
+    suncheck++;
+}
+}
+}
 
 
 $(document).on("pageinit","#findMatches2",function(){ 
@@ -188,4 +267,39 @@ $(document).on("pageinit","#findMatches2",function(){
 
 
 });
+
+function clearPage() {
+document.getElementById('head1').innerHTML = '';
+document.getElementById('head2').innerHTML = '';
+document.getElementById('head3').innerHTML = '';
+document.getElementById('head4').innerHTML = '';
+document.getElementById('head5').innerHTML = '';
+document.getElementById('head6').innerHTML = '';
+document.getElementById('head7').innerHTML = '';
+document.getElementById('submit1').innerHTML = '';
+document.getElementById('submit2').innerHTML = '';
+document.getElementById('submit3').innerHTML = '';
+document.getElementById('submit4').innerHTML = '';
+document.getElementById('submit5').innerHTML = '';
+document.getElementById('submit6').innerHTML = '';
+document.getElementById('submit7').innerHTML = '';
+
+$("#driver").attr("checked",false).checkboxradio("refresh");
+$("#passenger").attr("checked",false).checkboxradio("refresh");
+
+
+$("#mondayC").attr("checked", false).checkboxradio("refresh");
+$("#tuesdayC").attr("checked", false).checkboxradio("refresh");
+$("#wednesdayC").attr("checked", false).checkboxradio("refresh");
+$("#thursdayC").attr("checked", false).checkboxradio("refresh");
+$("#fridayC").attr("checked", false).checkboxradio("refresh");
+$("#saturdayC").attr("checked", false).checkboxradio("refresh");
+$("#sundayC").attr("checked", false).checkboxradio("refresh");
+
+
+} 
+
+$(document).on("pageshow","#myRideSchedule",function() {
+clearPage();
+})
 
